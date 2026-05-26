e5d4656c730ac24b9fc0f2becc0944c626ee1700
hotea <oksukai@gmail.com>
2026-05-26 16:54:46 +0800

    chore: merge SecretKey changes into v0.3.6, no new version

--- client/http_client.go
+++ client/http_client.go
@@ -18,7 +18,7 @@ const (
 	// UserAgentPrefix User-Agent 前缀
 	UserAgentPrefix = "openapi-go-sdk-"
 	// SDKVersion SDK 版本号
-	SDKVersion = "0.3.7"
+	SDKVersion = "0.3.6"
 	// DefaultCharset 默认字符集
 	DefaultCharset = "UTF-8"
 	// DefaultSignType 默认签名类型
